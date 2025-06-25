50b7d806e3a8a46970484466eb1a391c43cd7f2c
AlexanderPoklad <alexanderad@mail.ru>
2025-06-25 16:12:14 +0300

    lab3

--- src/itmo/java/basics/lesson5/Censored.java
+++ src/itmo/java/basics/lesson5/Censored.java
@@ -4,11 +4,19 @@ package itmo.java.basics.lesson5;
 //задание 3
 
 
+import java.util.Scanner;
+
 public class Censored {
     public static void main(String[] args) {
-        String text = "Автомобиль практически не использовался бяка и хранился большую часть времени в гараже.";
+        String text = getText();
         String censoredText = censorBannedWord(text);
         System.out.println(censoredText);
+    }
+
+    public static String getText(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите текст:");
+        return scanner.nextLine();
     }
 
     public static String censorBannedWord(String text) {
